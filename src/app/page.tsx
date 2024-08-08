e2c4385dd5addb7a719c25362c2fd00efa5a8843
DESKTOP-NEW\admin <minh.hoangcong@amela.vn>
2024-08-08 17:22:59 +0700

    add slider 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,26 @@
 import MainLayout from "@/app/layouts/MainLayout";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
     <MainLayout>
+      <div className="container">
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>123</CarouselItem>
+            <CarouselItem>456</CarouselItem>
+            <CarouselItem>789</CarouselItem>
+          </CarouselContent>
+        </Carousel>
+      </div>
       <div>Home page</div>
       <Button asChild>
         <Link href="/checkout">go to checkout</Link>
--- src/icons/SearchIcon.tsx
+++ src/icons/SearchIcon.tsx
@@ -10,16 +10,16 @@ export default function SearchIcon() {
       <path
         d="M11.1111 17.7222C15.0385 17.7222 18.2222 14.5385 18.2222 10.6111C18.2222 6.68375 15.0385 3.5 11.1111 3.5C7.18375 3.5 4 6.68375 4 10.6111C4 14.5385 7.18375 17.7222 11.1111 17.7222Z"
         stroke="#25282B"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
       <path
         d="M20.0002 19.5L16.1335 15.6334"
         stroke="#25282B"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
     </svg>
   );
