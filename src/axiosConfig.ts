f5be0911ee13eae4a3c2f083f3606aba5c4a6f92
DESKTOP-NEW\admin <minh.hoangcong@amela.vn>
2024-10-16 09:11:44 +0700

    update call api from server

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
